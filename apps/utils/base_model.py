d4b53eecabec02d95654491b9416611d841f6e17
sorena <mdn1376@gmail.com>
2024-10-24 12:20:43 +0330

    feat: remove field active

--- apps/utils/base_model.py
+++ apps/utils/base_model.py
@@ -4,7 +4,6 @@ from django.db import models
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
-    active = models.BooleanField(default=True, verbose_name='Active')
 
     class Meta:
         abstract = True
