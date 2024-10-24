7e2dd5e97ed2a1c6e5df87397dd8ed03d71b45ba
sorena <mdn1376@gmail.com>
2024-10-24 10:16:20 +0330

    faet: added field user_type
    added this field instead of is_admin for choice between admin & regular users

--- apps/accounts/managers.py
+++ apps/accounts/managers.py
@@ -13,7 +13,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, phone_number, password):
         user = self.create_user(phone_number, password)
-        user.is_admin = True
+        user.user_type = 'admin'
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -6,13 +6,20 @@ from apps.accounts.validators import phone_regex_validator
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    REGULAR = 'regular'
+    ADMIN = 'admin'
+
+    USER_TYPE_CHOICES = [
+        (REGULAR, 'Regular'),
+        (ADMIN, 'Admin'),
+    ]
+
     phone_number = models.CharField(max_length=12, unique=True, validators=[phone_regex_validator],
                                     verbose_name="Phone Number")
-
-    is_admin = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default=REGULAR, verbose_name="User Type")
+    is_superuser = models.BooleanField(default=False, verbose_name="Superuser")
+    is_staff = models.BooleanField(default=False, verbose_name="Staff")
+    is_active = models.BooleanField(default=True, verbose_name="Active")
 
     objects = UserManager()
 
