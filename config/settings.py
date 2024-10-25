277e48d5daa47351d55c9204658eb1c309236a53
sorena <mdn1376@gmail.com>
2024-10-25 10:01:22 +0330

    fix: removed unused filter backend in settings

--- config/settings.py
+++ config/settings.py
@@ -141,8 +141,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
-        'rest_framework.filters.OrderingFilter',
-        'rest_framework.filters.SearchFilter',
     ),
 }
 
